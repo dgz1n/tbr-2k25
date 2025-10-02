cc17889b7644624f9d5ee55754d4bf912dcce491
dgz1n <lima.pedro.douglas@gmail.com>
2025-10-02 15:45:17 -0300

    Add files via upload

--- Final_Code.py
+++ Final_Code.py
@@ -3,22 +3,27 @@ from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSenso
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
-from Drive_Train import andar, rotate, turn
 from consts import Open, close, low, mid, deposit, cancela_esquerda, cancela_direita, volta_direita, volta_esquerda
+from Drive_Train import claw, andar, turn, rotate, cancela
 from functions import detect_train_I, detect_train_II, detect_train_III, detect_train_IV
+from Rotas import route_I, route_II, route_III, route_IV, train_I, train_III, train_IV, boxG, boxB, park
 
 hub = PrimeHub()
+#linhas 25;26;27 não fazem parte de uma rota
 
-# rota 2
-#rotate(90)
-#andar(61)
-#rotate(90)
-#andar(14.2)
-
-
-#low()andar(-15)rotate(90)andar(50)rotate(-90)andar(44)rotate(90)andar(3)Open()
+route_I()
+train_I()
+detect_train_I()
+route_II()
 low()
-wait(300)
-andar(20)
+route_III()
+detect_train_II()
+boxG()
+route_IV()
+boxB()
+train_III()
+cancela_esquerda()
 rotate(-90)
-andar(32)
+volta_esquerda()
+detect_train_IV()
+park()
